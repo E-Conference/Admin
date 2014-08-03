add6cf91d54200f39d12361e592000266d7ac566
Vincent Sébille <vincent.sebille@gmail.com>
2014-08-03 15:47:37 +0200

    [Admin v1.0.0] Ajout du fichier d'initialisation

---
+++ init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "Init script for LiveCon Admin is strating...";
+echo "Enter the path of the fibe directory in Livecon repository (for the symbolik link) :";
+
+#read fibeDir;
+#ln -s ${fibeDir} ./src/test;
+
+echo "Configuration of the parameters.yml.";
+echo "database_host (ex : 127.0.0.1):";
+read database_host;
+echo "database_name (ex : wwwlivecon):";
+read database_name;
+echo "database_user (ex : liveon_app):";
+read database_user;
+echo "database_password (ex : liveon_app):";
+read database_password;
+
+echo "# This file is auto-generated during the composer install
+parameters:
+    database_driver: pdo_mysql
+    database_host: $database_host
+    database_port: null
+    database_name: $database_name
+    database_user: $database_user
+    database_password: $database_password
+    mailer_transport: smtp
+    mailer_host: 127.0.0.1
+    mailer_user: null
+    mailer_password: null
+    locale: en
+    secret: ThisTokenIsNotSoSecretChangeIt
+    debug_toolbar: true
+    debug_redirects: true
+    use_assetic_controller: true
+" > ./app/config/parameters.yml
+
+echo "End of Initialisation.";
+echo "";
+echo "don't forget to do the following commands:"
+echo "composer install";
+echo "php app/console assets:install";
+echo "php app/console cache:clear";
